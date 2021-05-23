7faf623f1ce1048f3dc2a61d44725da4d3bcfe26
kkjot88 <kajotpp@gmail.com>
2021-05-23 21:48:11 +0200

    Added new blank class PlikZAdresatami

---
+++ PlikZAdresatami.h
@@ -0,0 +1,22 @@
+#ifndef PLIKZADRESATAMI_H
+#define PLIKZADRESATAMI_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+
+#include "Adresat.h"
+#include "MetodyPomocnicze.h"
+
+using namespace std;
+
+class PlikZAdresatami {
+	
+public:
+	
+};
+
+#endif
+
